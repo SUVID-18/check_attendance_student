1693beed14f054418a58b95e5de3c5ca63cecdfa
dongminii <h990309@naver.com>
2023-04-06 21:46:25 +0900

    메인페이지 1차수정 #10번.

--- lib/view/main_page.dart
+++ lib/view/main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../main.dart';
+
 /// 홈 화면을 나타내는 페이지 입니다.
 ///
 /// 홈 화면에서는 기본적으로 출결을 위한 태그 인식하기 버튼을 누를 수 있으며, 현재 출석을 하는 과목이 표시됩니다.
@@ -11,100 +13,103 @@ class MainPage extends StatelessWidget {
 
   const MainPage({required this.appName, Key? key}) : super(key: key);
 
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('페이지 이동 버튼모음')),
-        body: Column(
-          children: const <Widget>[
-            Spacer(),
-            ElevatedCardExample(),
-            FilledCardExample(),
-            OutlinedCardExample(),
-            ElevatedCardExample_2(),
-            Spacer(),
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(appName),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0), // 컨텐츠 가로 여백 추가
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // 컨텐츠를 수직 가운데 정렬
+          crossAxisAlignment: CrossAxisAlignment.stretch, // 컨텐츠를 수평으로 꽉 채우기
+          children: [
+            Image(image: NetworkImage('https://www.suwon.ac.kr/usr/images/suwon/ui_img09.png')),
+            Text('페이지 이동 버튼 모음',
+              style: Theme.of(context).textTheme.headline4, // headlineLarge -> headline4 로 변경
+              textAlign: TextAlign.center, // 텍스트를 중앙 정렬
+            ),
+
+
+            SizedBox(height: 32), // 버튼과 텍스트 사이 여백 추가
+            MaterialButton(
+              child: Text('로그인/로그아웃 페이지로'),
+              onPressed: () => context.push('/login'),
+              minWidth: double.infinity, // 버튼의 최소 너비를 부모 컨테이너와 같게 설정
+              height: 64, // 버튼의 고정된 높이 설정
+              color: Colors.blue, // 버튼 색상 변경
+              textColor: Colors.white, // 버튼 텍스트 색상 변경
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // 버튼의 모서리 둥글게 처리
+              ),
+            ),
+            SizedBox(height: 16), // 버튼과 버튼 사이 여백 추가
+
+
+            MaterialButton(
+              child: Text('디바이스 등록 페이지로'),
+              onPressed: () => context.push('/settings/register_device'),
+              minWidth: double.infinity,
+              height: 64,
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            SizedBox(height: 16),
+
+
+
+            MaterialButton(
+              child: Text('출결 기록 페이지로'),
+              onPressed: () => context.push('/history'),
+              minWidth: double.infinity,
+              height: 64,
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            SizedBox(height: 16),
+
+
+            MaterialButton(
+              child: Text('설정 페이지로'),
+              onPressed: () => context.push('/settings'),
+              minWidth: double.infinity,
+              height: 64,
+              color: Colors.blue,
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            SizedBox(height: 16),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ElevatedCardExample extends StatelessWidget {
-  const ElevatedCardExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Card(
-        child: SizedBox(
-          width: 300,
-          height: 100,
-          child: Center(child: Text('로그인 페이지로')),
-        ),
-      ),
-    );
-  }
-}
-
-class FilledCardExample extends StatelessWidget {
-  const FilledCardExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        elevation: 0,
-        color: Theme.of(context).colorScheme.surfaceVariant,
-        child: const SizedBox(
-          width: 300,
-          height: 100,
-          child: Center(child: Text('설정 페이지로')),
-        ),
-      ),
-    );
-  }
-}
-
-class OutlinedCardExample extends StatelessWidget {
-  const OutlinedCardExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-          side: BorderSide(
-            color: Theme.of(context).colorScheme.outline,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-        ),
-        child: const SizedBox(
-          width: 300,
-          height: 100,
-          child: Center(child: Text('디바이스 등록 페이지로')),
-        ),
-      ),
-    );
-  }
-}
-
-class ElevatedCardExample_2 extends StatelessWidget {
-  const ElevatedCardExample_2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Card(
-        child: SizedBox(
-          width: 300,
-          height: 100,
-          child: Center(child: Text('출결 기록 페이지')),
         ),
       ),
     );
