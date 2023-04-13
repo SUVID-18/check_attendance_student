84e2babdb9e5baea930afee6f3d728c8e0bd1a72
sun30812 <sun30812@naver.com>
2023-04-13 10:01:39 +0900

    #10 로그인 화면 동작, 기기 변경 페이지 동작 viewModel을 싱글톤 클래스로 변경

--- lib/view_model/login.dart
+++ lib/view_model/login.dart
@@ -2,27 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 /// 로그인 페이지의 동작을 담당하는 클래스
-///
-/// 로그인 페이지에서 필요한 부분에 대입시키면 된다.
-///
-/// ```dart
-///var viewModel = LoginViewModel();
-///TextField(
-///             controller: viewModel.usernameController,
-///             decoration: InputDecoration(
-///                 filled: true,
-///                 labelText: 'ID'
-///             ),
-///           ),
-///```
 class LoginViewModel {
   /// 사용자 ID에 해당되는 컨트롤러
   final usernameController = TextEditingController();
+
   /// 사용자 비밀번호에 해당되는 컨트롤러
   final passwordController = TextEditingController();
 
-  /// 로그인 버튼을 누른 경우 수행할 동작
+  static final LoginViewModel _instance = LoginViewModel._init();
+
+  /// 로그인 페이지의 동작을 담당하는 클래스의 생성자
   ///
+  /// 로그인 페이지에서 필요한 부분에 대입시키면 된다.
+  ///
+  /// ```dart
+  ///var viewModel = LoginViewModel();
+  ///TextField(
+  ///             controller: viewModel.usernameController,
+  ///             decoration: InputDecoration(
+  ///                 filled: true,
+  ///                 labelText: 'ID'
+  ///             ),
+  ///           ),
+  ///```
+  factory LoginViewModel() => _instance;
+
+  /// 로그인 버튼을 누른 경우 수행할 동작
+
   /// 로그인 버튼의 `onPressed`에 해당 메서드를 등록하면 된다.
   /// ID와 비밀번호가 비어있을 시 [loginBlankDialog]에 제공된 `Dialog`를 띄우고
   /// 정상 입력을 할 시 로그인이 진행된다. 본 메서드는 [showDialog]메서드를 포함하기에
@@ -51,9 +57,8 @@ class LoginViewModel {
       );
     }
   }
-
   /// 로그인 페이지 하단에 기기변경 신청 버튼 누를 시 동작
-  ///
+
   /// 기기변경 신청 버튼의 `onPressed`에 해당 메서드를 등록하면 된다.
   /// 페이지 이동을 위해 [context]가 필요하다.
   ///
@@ -68,4 +73,6 @@ class LoginViewModel {
   ///```
   void onChangePressed(BuildContext context) =>
       context.push('/register_device');
+
+  LoginViewModel._init();
 }
--- lib/view_model/register_device.dart
+++ lib/view_model/register_device.dart
@@ -17,10 +17,32 @@ import 'package:flutter/material.dart';
 class RegisterDeviceViewModel {
   /// 이름에 해당되는 컨트롤러
   final nameController = TextEditingController();
+
   /// 전화번호에 해당되는 컨트롤러
   final phoneNumberController = TextEditingController();
+
   /// 학번에 해당되는 컨트롤러
   final studentIDController = TextEditingController();
+
+  static final RegisterDeviceViewModel _instance =
+      RegisterDeviceViewModel._init();
+
+  /// 기기변경 페이지의 동작을 담당하는 생성자
+  ///
+  /// 기기변경 페이지에서 필요한 부분에 대입시키면 된다.
+  ///
+  /// ```dart
+  ///var viewModel = RegisterDeviceViewModel();
+  ///TextField(
+  ///        controller: viewModel.nameController,
+  ///        decoration: InputDecoration(
+  ///            filled: true,
+  ///            labelText: 'ID'
+  ///        ),
+  ///      ),
+  ///```
+  factory RegisterDeviceViewModel() => _instance;
+
   /// 기기변경 페이지에서 기기변경 버튼을 누른 경우 수행할 동작
   ///
   /// 기기변경 버튼의 `onPressed`에 해당 메서드를 등록하면 된다.
@@ -44,7 +66,9 @@ class RegisterDeviceViewModel {
   ///```
   void onSubmitPressed(
       {required BuildContext context, required AlertDialog loginBlankDialog}) {
-    if (nameController.text.isEmpty || phoneNumberController.text.isEmpty || studentIDController.text.isEmpty) {
+    if (nameController.text.isEmpty ||
+        phoneNumberController.text.isEmpty ||
+        studentIDController.text.isEmpty) {
       showDialog(
         context: context,
         builder: (context) => loginBlankDialog,
@@ -52,4 +76,5 @@ class RegisterDeviceViewModel {
     }
   }
 
+  RegisterDeviceViewModel._init();
 }
