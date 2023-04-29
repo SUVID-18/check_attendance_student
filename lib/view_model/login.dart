313da4297620230ac26227498a4e450426252cbc
sun30812 <sun30812@naver.com>
2023-04-29 13:26:58 +0900

    #4 로그인 성공 시 바로 메인 화면으로 넘어가도록 구성

--- lib/view_model/login.dart
+++ lib/view_model/login.dart
@@ -2,11 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// 로그인 페이지의 동작을 담당하는 클래스
 class LoginViewModel with WidgetsBindingObserver {
   /// 사용자 ID에 해당되는 컨트롤러
   TextEditingController get emailController => _emailController;
+
+  /// 로그인 관련 이벤트 발생 시 위젯을 띄우기 위한 [BuildContext]
+  BuildContext context;
 
   /// 로그인 오류 여부 확인
   bool get isLoginError => _isLoginError;
@@ -32,8 +36,12 @@ class LoginViewModel with WidgetsBindingObserver {
   /// 로그인 링크 클릭을 인식하기 위해서는 아래 코드처럼 [initState]와
   /// [dispose]에 일부 코드 추가가 필요하다.
   ///
+  /// 또한 로그인 관련 이벤트 발생 시 위젯을 띄우기 위해 [context]에 [BuildContext]를 전달해야 한다.
+  /// [BuildContext]는 화면 구성 뒤에 생성되기에 `late`키워드를 붙여서 [context]에 넘겨지는 값이
+  /// 추후 유효하다는 것을 명시해야한다.
+  ///
   /// ```dart
-  ///var viewModel = LoginViewModel();
+  ///late var viewModel = LoginViewModel(context: context);
   ///
   /// @override
   /// void initState() {
@@ -55,34 +63,86 @@ class LoginViewModel with WidgetsBindingObserver {
   ///             ),
   ///           ),
   ///```
-  factory LoginViewModel() => LoginViewModel._init();
+  factory LoginViewModel({required BuildContext context}) =>
+      LoginViewModel._init(context: context);
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
-    FirebaseDynamicLinks.instance.onLink.listen((event) async {
-      final Uri deepLink = event.link;
-      if (FirebaseAuth.instance.isSignInWithEmailLink(deepLink.toString())) {
-        try {
-          FirebaseAuth.instance.signInWithEmailLink(
-              email: _emailController.text, emailLink: deepLink.toString());
-        } catch (_) {
+    if ((state != AppLifecycleState.resumed) &&
+        _emailController.text.isNotEmpty) {
+      SharedPreferences.getInstance().then((pref) async {
+        await pref.setString('userEmail', _emailController.text);
+      });
+    } else if (state == AppLifecycleState.resumed) {
+      try {
+        FirebaseDynamicLinks.instance.onLink.listen((event) {
+          _passwordlessLogin(event);
+        });
+      } catch (error) {
+        // 사실상 뜨면 안되는 오류
+        print(error);
+      }
+    }
+  }
+
+  void _passwordlessLogin(PendingDynamicLinkData event) async {
+    if (_emailController.text.isNotEmpty &&
+        FirebaseAuth.instance.isSignInWithEmailLink(event.link.toString())) {
+      showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                CircularProgressIndicator.adaptive(),
+                Text('  로그인 중...')
+              ],
+            ),
+          ),
+        ),
+      );
+      try {
+        await FirebaseAuth.instance
+            .signInWithEmailLink(
+                email: _emailController.text, emailLink: event.link.toString())
+            .then((_) {});
+
+        if (context.mounted) {
+          Navigator.pop(context);
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: const Text('로그인되었습니다.'),
+              duration: const Duration(seconds: 1),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0))));
+          context.go('/');
+        }
+      } catch (err) {
+        Navigator.pop(context);
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: const Text('로그인에 실패하였습니다.'),
+              duration: const Duration(seconds: 1),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0))));
           _isLoginError = true;
         }
       }
-    });
+    }
   }
 
   /// 로그인 버튼을 누른 경우 수행할 동작
 
   /// 로그인 버튼의 `onPressed`에 해당 메서드를 등록하면 된다.
   /// ID와 비밀번호가 비어있을 시 [loginBlankDialog]에 제공된 `Dialog`를 띄우고
-  /// 정상 입력을 할 시 로그인이 진행된다. 본 메서드는 [showDialog]메서드를 포함하기에
-  /// [context]에 [BuildContext]를 넘겨주여야 한다.
+  /// 정상 입력을 할 시 로그인이 진행된다.
   ///
   /// ```dart
   /// ElevatedButton(
   ///      onPressed: () => viewModel.onSubmitPressed(
-  ///          context: context,
   ///          loginBlankDialog: AlertDialog(
   ///              title: Text('안내'),
   ///              content: Text('ID와 비밀번호를 모두 입력하십시요'),
@@ -93,8 +153,7 @@ class LoginViewModel with WidgetsBindingObserver {
   ///              ])),
   ///      child: Text('Next'))
   ///```
-  void onSubmitPressed(
-      {required BuildContext context, required AlertDialog loginBlankDialog}) {
+  void onSubmitPressed({required AlertDialog loginBlankDialog}) {
     if (_emailController.text.isEmpty) {
       showDialog(
         context: context,
@@ -154,7 +213,6 @@ class LoginViewModel with WidgetsBindingObserver {
   /// 로그인 페이지 하단에 기기변경 신청 버튼 누를 시 동작
 
   /// 기기변경 신청 버튼의 `onPressed`에 해당 메서드를 등록하면 된다.
-  /// 페이지 이동을 위해 [context]에 [BuildContext]를 전잘해야 한다.
   ///
   /// ```dart
   /// ElevatedButton(
@@ -162,11 +220,22 @@ class LoginViewModel with WidgetsBindingObserver {
   ///          '기기변경 신청',
   ///          style: TextStyle(fontSize: 15),
   ///        ),
-  ///        onPressed: () => viewModel.onChangePressed(context),
+  ///        onPressed: () => viewModel.onChangePressed(),
   ///      ),
   ///```
-  void onChangePressed(BuildContext context) =>
-      context.push('/register_device');
+  void onChangePressed() => context.push('/register_device');
 
-  LoginViewModel._init();
+  LoginViewModel._init({required this.context}) {
+    SharedPreferences.getInstance().then((pref) {
+      var email = pref.getString('userEmail');
+      if (email != null) {
+        _emailController.text = email;
+      }
+    });
+    FirebaseDynamicLinks.instance.getInitialLink().then((initialLink) {
+      if (initialLink != null) {
+        _passwordlessLogin(initialLink);
+      }
+    });
+  }
 }
--- lib/view_model/settings_page.dart
+++ lib/view_model/settings_page.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// 설정 페이지의 동작을 담당하는 클래스
 class SettingsPageViewModel {
+  /// 특정 메서드에서 위젯을 띄우기 위한 [BuildContext]
+  BuildContext context;
+
   /// 사용자 이름에 해당되는 읽기전용 속성
   String get userName => _userName;
 
@@ -21,14 +25,16 @@ class SettingsPageViewModel {
   late final String _userMajor;
   final String _userEmail =
       FirebaseAuth.instance.currentUser?.email ?? '이메일 없음';
-  static final _instance = SettingsPageViewModel._init();
 
   /// 설정 페이지의 동작을 담당하는 생성자
   ///
   /// 설정 페이지에서 필요한 부분에 대입시키면 된다.
+  /// 또한 특정 메서드에서 위젯을 띄우기 위해 [context]에 [BuildContext]를 전달해야 한다.
+  /// [BuildContext]는 화면 구성 뒤에 생성되기에 `late`키워드를 붙여서 [context]에 넘겨지는 값이
+  /// 추후 유효하다는 것을 명시해야한다.
   ///
   /// ```dart
-  ///var viewModel = RegisterDeviceViewModel();
+  ///late var viewModel = SettingsPageViewModel(context: context);
   ///  Card(
   ///     child: Column(
   ///       children: [
@@ -39,17 +45,46 @@ class SettingsPageViewModel {
   ///     ),
   ///   )
   ///```
-  factory SettingsPageViewModel() => _instance;
+  factory SettingsPageViewModel({required BuildContext context}) =>
+      SettingsPageViewModel._init(context: context);
 
   /// 로그아웃을 하는 메서드
   ///
   /// 로그인된 계정을 로그아웃 할 시 사용되는 메소드이다.
-  /// [context]에 [BuildContext]를 필요로 한다.
-  Future<void> logout({required BuildContext context}) async {
-    FirebaseAuth.instance.signOut().then((_) => context.go('/'));
+  ///
+  /// ```dart
+  /// ElevatedButton(
+  ///  onPressed: () {
+  ///    viewModel.logout();
+  ///  },
+  ///  child: const Text('로그아웃'))
+  void logout() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => Dialog(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: const [
+              CircularProgressIndicator.adaptive(),
+              Text('  로그아웃 중...')
+            ],
+          ),
+        ),
+      ),
+    );
+    FirebaseAuth.instance
+        .signOut()
+        .then((_) => SharedPreferences.getInstance().then((pref) {
+              pref.remove('userEmail').then((_) {
+                Navigator.pop(context);
+                context.go('/');
+              });
+            }));
   }
 
-  SettingsPageViewModel._init() {
+  SettingsPageViewModel._init({required this.context}) {
     _userName = '';
     _studentId = '';
     _userMajor = '';
