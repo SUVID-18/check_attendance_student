95b3b000c538e75624087498045362800bf47aae
sun30812 <sun30812@naver.com>
2023-05-01 11:04:33 +0900

    #3 기기변경 요청 기능 구현
    학생 토큰에 해당되는 `attendanceStudentId`는 직접 접근으로만 접근이 가능하도록 구성 -> `Student.fromJson`으로 학생 정보를 서버에서 가져올 수는 있지만 그 때 가져오는 토큰 정보는 무의미한 값.

--- test/model_test.dart
+++ test/model_test.dart
@@ -4,34 +4,20 @@ import 'package:check_attendance_student/model/attendance_information.dart';
 import 'package:check_attendance_student/model/lecture.dart';
 import 'package:check_attendance_student/model/student.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:uuid/uuid.dart';
 
 void main() {
   group('학생 객체 테스트', () {
     test('학생 객체가 정상적으로 생성되는지 테스트', () {
       final student = Student(
           studentId: '18017xxx',
-          attendanceStudentId: const Uuid().v4(),
           department: '컴퓨터학부',
           subject: '컴퓨터 SW',
           name: '수원대');
       expect(student.name, '수원대');
     });
-    test('학생 객체의 학생 식별용 UUID 변경 테스트', () {
-      final student = Student(
-          studentId: '18017xxx',
-          attendanceStudentId: const Uuid().v4(),
-          department: '컴퓨터학부',
-          subject: '컴퓨터 SW',
-          name: '수원대');
-      var newUuid = const Uuid().v4();
-      var newStudent = student.updateStudentUuid(newUuid);
-      expect(newStudent.attendanceStudentId, newUuid);
-    });
     test('학생 객체의 직렬화 테스트', () {
       final student = Student(
           studentId: '18017xxx',
-          attendanceStudentId: const Uuid().v4(),
           department: '컴퓨터학부',
           subject: '컴퓨터 SW',
           name: '수원대');
