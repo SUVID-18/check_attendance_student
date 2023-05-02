b777c146aa3dc47070cee90de289eebc214f5fb4
Planybirth <splashslayer@gmail.com>
2023-05-02 22:31:18 +0900

    오류 수정 및 view파일 롤백.

--- lib/view/attendance.dart
+++ lib/view/attendance.dart
@@ -2,6 +2,7 @@ import 'package:check_attendance_student/view/styles.dart';
 import 'package:flutter/material.dart';
 
 /// 태그 시 간단한 강의실 정보와 출석하기 버튼을 출력하는 페이지입니다.
+/// parameter로 uuid를 전달받습니다.
 class AttendancePage extends StatelessWidget {
   final String uuid;
   const AttendancePage({required this.uuid, Key? key}) : super(key: key);
@@ -26,7 +27,6 @@ class AttendancePage extends StatelessWidget {
                 lectureName: '찾을 수 없음',
               ),
             ),
-            if (uuid != null) Text('uuid : $uuid'),
           ],
         ),
       ),
