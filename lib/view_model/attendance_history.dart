80281831b2f2e717f2d3b319f6d7e7e946f5b971
sun30812 <sun30812@naver.com>
2023-05-11 09:12:16 +0900

    #10 출결 기록페이지 뷰모델에 누락된 주석 추가

--- lib/view_model/attendance_history.dart
+++ lib/view_model/attendance_history.dart
@@ -2,6 +2,7 @@ import 'package:check_attendance_student/model/attendance_information.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 /// 출결 기록 페이지의 동작을 담당하는 클래스
 class AttendanceHistoryViewModel {
@@ -13,7 +14,7 @@ class AttendanceHistoryViewModel {
   /// 데이터가 추가될 변수이다.
   ///
   /// 직접 수정이 불가능하다.
-  @Deprecated('해당 메서드는 더 이상 사용할 수 없습니다. ')
+  @Deprecated('해당 메서드는 더 이상 사용할 수 없습니다. 대신 getAttendanceHistory 메서드를 사용하세요.')
   List<AttendanceInformation> get attendanceHistoryList =>
       _attendanceHistoryList;
   final List<AttendanceInformation> _attendanceHistoryList = [
@@ -31,6 +32,18 @@ class AttendanceHistoryViewModel {
         result: AttendanceResult.normal),
   ];
 
+  /// 학생 본인의 출결 기록 리스트를 불러오는 메서드
+  ///
+  /// 강의실 번호, 과목명, 교수 이름, 출석여부가 포함되어있는 리스트를 서버로부터
+  /// 불러오는 메서드이다. [Future]형태이기 때문에 반환값을 얻기 위해서는
+  /// [FutureBuilder]가 필요하다.
+  ///
+  /// 만일 로그인이 안된 상태로 해당 메서드를 호출하는 경우 로그인이 되어있지 않다는 메세지와 함께 오류가 발생한다.
+  ///
+  /// ## 같이보기
+  /// * [AttendanceInformation]
+  /// * [FutureBuilder]
+  ///
   Future<List<AttendanceInformation>> getAttendanceHistory() async {
     var user = FirebaseAuth.instance.currentUser;
     if (user != null) {
