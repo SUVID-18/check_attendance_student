0c47056c6653ad3020562b4ab6cf2af66eb4870b
dongminii <h990309@naver.com>
2023-05-15 22:38:20 +0900

    styles 페이지 초안

--- lib/view/styles.dart
+++ lib/view/styles.dart
@@ -3,27 +3,52 @@ import 'package:flutter/material.dart';
 /// 강의실 정보와 출석하기 버튼이 포함된 위젯
 ///
 /// 강의실 이름과 과목 이름과 출결하기 버튼을 출력하는 카드 형태의 위젯이다.
-class CheckAttendanceCard extends StatelessWidget {
-  /// 강의실 이름에 해당되는 변수
+/// 강의실 이름에 해당되는 변수
+//lectureRoomName;
+/// 과목 이름에 해당되는 변수
+//lectureName;
+/// 출석하기 버튼을 누를 시 수행되는 동작
+//onAttendance;
+/// 강의실 정보와 출석하기 버튼이 포함된 위젯
+///
+/// 강의실 이름에 대한 정보를 [lectureRoomName]를 통해 전달받으며, 과목 이름에 대한 정보를
+/// [lectureName]을 통해 전달받는 카드 형태의 위젯으로 출석하기 버튼이 포함되어있다.
+/// 출석하기 버튼을 누를 시 수행될 동작은 [onAttendance]를 통해 제공하면 된다.
+
+/// ///CheckAttendanceCard 위젯을 StatefulWidget으로 변경하여 내부 상태를 관리.
+///_CheckAttendanceCardState 클래스에서 _isButtonEnabled라는 변수를 추가하여 출석 버튼의 활성화 상태를 추적.
+///_handleAttendance 메서드는 버튼이 활성화되어 있고 onAttendance 콜백이 제공된 경우에만 호출됨
+///이후 버튼을 비활성화하고 onAttendance 콜백을 실행.
+///출석 버튼의 onPressed 콜백과 버튼 스타일에 _isButtonEnabled 변수를 사용하여 버튼의 활성화 상태에 따라 색상을 조정.
+/// 번 출석 버튼을 누르면 버튼이 비활성화되도록 구현함.
+
+class CheckAttendanceCard extends StatefulWidget {
   final String lectureRoomName;
-
-  /// 과목 이름에 해당되는 변수
   final String lectureName;
-
-  /// 출석하기 버튼을 누를 시 수행되는 동작
   final void Function()? onAttendance;
 
-  /// 강의실 정보와 출석하기 버튼이 포함된 위젯
-  ///
-  /// 강의실 이름에 대한 정보를 [lectureRoomName]를 통해 전달받으며, 과목 이름에 대한 정보를
-  /// [lectureName]을 통해 전달받는 카드 형태의 위젯으로 출석하기 버튼이 포함되어있다.
-  /// 출석하기 버튼을 누를 시 수행될 동작은 [onAttendance]를 통해 제공하면 된다.
   const CheckAttendanceCard({
     required this.lectureRoomName,
     required this.lectureName,
     this.onAttendance,
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _CheckAttendanceCardState createState() => _CheckAttendanceCardState();
+}
+
+class _CheckAttendanceCardState extends State<CheckAttendanceCard> {
+  bool _isButtonEnabled = true;
+
+  void _handleAttendance() {
+    if (_isButtonEnabled && widget.onAttendance != null) {
+      setState(() {
+        _isButtonEnabled = false;
+      });
+      widget.onAttendance!();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,39 +58,47 @@ class CheckAttendanceCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
               child: Row(
-                children: const [
+                children: [
                   Icon(Icons.school_outlined),
-                  Text('  출석체크',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 23.0))
+                  Text(
+                    '  출석체크',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23.0,
+                    ),
+                  ),
                 ],
               ),
             ),
-            Text('강의실 이름: $lectureRoomName',
-                style: Theme.of(context).textTheme.headlineSmall),
             Text(
-              '과목 이름: $lectureName',
-              style: Theme.of(context).textTheme.headlineSmall,
+              '강의실 이름: ${widget.lectureRoomName}',
+              style: Theme.of(context).textTheme.headline6,
             ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width - 300,
+            Text(
+              '과목 이름: ${widget.lectureName}',
+              style: Theme.of(context).textTheme.headline6,
             ),
-            // 출석을 진행하는 동작을 수행하는 버튼
+            const SizedBox(height: 20.0),
             OutlinedButton(
-                onPressed: onAttendance,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                    Text(' 출석하기')
-                  ],
-                )),
+              onPressed: _handleAttendance,
+              style: OutlinedButton.styleFrom(
+                backgroundColor:
+                    _isButtonEnabled ? null : Theme.of(context).disabledColor,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.check,
+                    color: _isButtonEnabled ? Colors.green : Colors.grey,
+                  ),
+                  const Text(' 출석하기'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
