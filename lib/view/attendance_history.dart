f4bee647fa2a9d673c1cdd1bde7f212ad07ccdbf
sun30812 <sun30812@naver.com>
2023-09-18 20:53:58 +0900

    #55 출결 기록을 불러오지 않는 문제 해결
    * Flutter 버전 업데이트에 따른 코드 변경사항 적용
    * 출결 기록을 가져올 때 올바른 메서드를 사용하도록 반영

--- lib/view/attendance_history.dart
+++ lib/view/attendance_history.dart
@@ -1,3 +1,4 @@
+import 'package:check_attendance_student/model/attendance_information.dart';
 import 'package:flutter/material.dart';
 
 import '../view_model/attendance_history.dart';
@@ -29,70 +30,89 @@ class _AttendanceHistoryPageState extends State<AttendanceHistoryPage> {
           )),
 
       //ListView를 사용해 리스트를 동적으로 나타내도록 함
-      body: ListView.builder(
-        ///리스트의 길이 만큼 카운트
-        itemCount: viewModel.attendanceHistoryList.length,
+      body: FutureBuilder<List<AttendanceInformation>>(
+          future: viewModel.getAttendanceHistory(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            } else if (snapshot.data == null) {
+              return const Center(
+                child: Text('출결 기록이 없습니다.'),
+              );
+            } else {
+              // 위에서 null여부 파악했으니 아래 코드부터는 무조건 null이 아니라 가정하고 코드 사용(!).
+              var attendanceHistoryList = snapshot.data!;
+              return ListView.builder(
+                ///리스트의 길이 만큼 카운트
+                itemCount: attendanceHistoryList.length,
 
-        ///위젯을 인덱스 만큼 만들도록 함
-        itemBuilder: (context, index) {
-          ///출결 한 기록을 탭하여 세부 정보를 볼 수 있는 gesturedetector
-          ///alertDialog를 통해 강의실 번호를 입력 받아 확인시 업로드가 된다
-          return GestureDetector(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                            title: const Text('출결 정보'),
-                            content: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                    '교수명: ${viewModel.attendanceHistoryList[index].professorName}',
-                                    style: const TextStyle(fontSize: 20.0),
-                                  ),
-                                  Text(
-                                    '과목명: ${viewModel.attendanceHistoryList[index].subjectName}',
-                                    style: const TextStyle(fontSize: 20.0),
-                                  ),
-                                  Text(
-                                    '출결 여부: ${viewModel.attendanceHistoryList[index].result}',
-                                    style: const TextStyle(fontSize: 20.0),
-                                  ),
-                                ]),
+                ///위젯을 인덱스 만큼 만들도록 함
+                itemBuilder: (context, index) {
+                  ///출결 한 기록을 탭하여 세부 정보를 볼 수 있는 gesturedetector
+                  ///alertDialog를 통해 강의실 번호를 입력 받아 확인시 업로드가 된다
+                  return GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                                    title: const Text('출결 정보'),
+                                    content: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            '교수명: ${attendanceHistoryList[index].professorName}',
+                                            style:
+                                                const TextStyle(fontSize: 20.0),
+                                          ),
+                                          Text(
+                                            '과목명: ${attendanceHistoryList[index].subjectName}',
+                                            style:
+                                                const TextStyle(fontSize: 20.0),
+                                          ),
+                                          Text(
+                                            '출결 여부: ${attendanceHistoryList[index].result}',
+                                            style:
+                                                const TextStyle(fontSize: 20.0),
+                                          ),
+                                        ]),
 
-                            ///확인 버튼임
-                            //아직 입력 받아 리스트에 추가하는것 구현 안함
-                            actions: <Widget>[
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('확인'))
-                            ]));
-              },
+                                    ///확인 버튼임
+                                    //아직 입력 받아 리스트에 추가하는것 구현 안함
+                                    actions: <Widget>[
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text('확인'))
+                                    ]));
+                      },
 
-              ///실제 나타나는 출결 목록들
-              child: Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      viewModel.attendanceHistoryList[index].subjectName,
-                      style: const TextStyle(fontSize: 20.0),
-                    ),
-                    Text(
-                      viewModel.attendanceHistoryList[index].professorName,
-                      style: const TextStyle(fontSize: 20.0),
-                    ),
-                    Text(
-                      viewModel.attendanceHistoryList[index].result.toString(),
-                      style: const TextStyle(fontSize: 20.0),
-                    ),
-                  ],
-                ),
-              ));
-        },
-      ),
+                      ///실제 나타나는 출결 목록들
+                      child: Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              attendanceHistoryList[index].subjectName,
+                              style: const TextStyle(fontSize: 20.0),
+                            ),
+                            Text(
+                              attendanceHistoryList[index].professorName,
+                              style: const TextStyle(fontSize: 20.0),
+                            ),
+                            Text(
+                              attendanceHistoryList[index].result.toString(),
+                              style: const TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                      ));
+                },
+              );
+            }
+          }),
     );
   }
 }
